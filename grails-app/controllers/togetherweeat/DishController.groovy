a210911c6a01fa523257644c8b4d64896e29a6f2
Yu Zhao <yzhao1@macalester.edu>
2013-02-09 05:53:10 -0600

    Adding Some Stuff

---
+++ grails-app/controllers/togetherweeat/DishController.groovy
@@ -0,0 +1,41 @@
+package togetherweeat
+
+class DishController {
+
+    def beforeInterceptor = [action: this.&auth]
+
+    private auth() {
+        if (!session.user) {
+            redirect(controller: "user", action: "login")
+            return false
+        }
+    }
+
+    def show() {
+        User u = User.get(session.user)
+        render(view: "show", model: [user:u])
+    }
+
+    def submitDishInfo() {
+
+        def dish = new Dish()
+        dish.setName(params.name)
+        dish.setFlavor(params.flavor)
+        dish.setType(params.type)
+        dish.setMaterial(params.material)
+        dish.setCooking_method(params.method)
+        dish.setAlert(params.alert)
+        dish.setRecipe(params.recipe)
+        dish.setCost(params.cost.toFloat())
+        dish.save(failOnError: true, flash: true)
+
+        Meal meal = Meal.get(session.meal)
+
+        def course = new Course();
+        course.setMeal(meal);
+        course.setDish(dish);
+        dish.save(failOnError: true, flash: true)
+
+        render(view: addDish);
+    }
+}
